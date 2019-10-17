69ff12c6fc90a2102c537d14933587fc16cbf721
DESKTOP-HQL3LME\Tiancheng Chen <tchen117@uottawa.ca>
2019-10-16 21:12:47 -0400

    add expect optional message

--- Test/PriorityQueue.js
+++ Test/PriorityQueue.js
@@ -15,12 +15,11 @@ describe("Test class Priority Queue", () => {
     queue.enqueue(['forth']);
     queue.enqueue('fifth');
     queue.enqueue(['sixth', 3]);
-    expect(queue.front()).to.deep.equal(['sixth', 3]);
-    console.log(queue.storage);
-    expect(queue.isEmpty()).to.equal(false);
+    expect(queue.front(), 'queue first item').to.deep.equal(['sixth', 3]);
+    expect(queue.isEmpty(), 'check if queue is empty').to.equal(false);
     expect(queue.size()).to.equal(6);
     expect(queue.dequeue()).to.deep.equal(['sixth', 3]);
-    expect(queue.size()).to.equal(5);
+    expect(queue.size(), 'queue size').to.equal(5);
     console.log(queue.storage);
   })
 })
--- Test/Stack.js
+++ Test/Stack.js
@@ -6,8 +6,8 @@ describe("Test class Stack", () => {
     let stack = new Stack();
 
     stack.push(1992);
-    expect(stack.peek()).equal(1992);
-    expect(stack.length()).equal(1);
+    expect(stack.peek(), 'stack last item').equal(1992);
+    expect(stack.length(), 'stack length').equal(1);
     let result = stack.pop();
     expect(result).equal(1992);
     expect(stack.peek()).to.be.undefined;
